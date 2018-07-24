b816cfa4e8bcec0c060eb46feddcfc40b751aed8
quinnHuang <quinn.huang@oocl.com>
2018-07-24 17:01:27 +0800

    [Quinn] replace postMapping to putMapping,patchMapping to postMapping

--- src/main/java/com/boot/emp/EmployeeController.java
+++ src/main/java/com/boot/emp/EmployeeController.java
@@ -12,25 +12,25 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/emp")
+    @GetMapping("/employees")
     public List<Employee> selectAll(){
         return employeeService.getEmpList();
     }
-    @GetMapping("/emp/{id}")
+    @GetMapping("/employees/{id}")
     public Employee selectById(@PathVariable int id){
         return employeeService.getEmpById(id);
     }
 
-    @PostMapping("/emp")
+    @PutMapping("/employees")
     public void update(@RequestBody Employee emp){
         employeeService.updateEmp(emp);
     }
 
-    @PatchMapping("/emp")
+    @PostMapping("/employees")
     public void addEmp(@RequestBody Employee emp){
         employeeService.add(emp);
     }
-    @DeleteMapping("/emp")
+    @DeleteMapping("/employees")
     public void deleteEmp(@RequestBody int id){
         employeeService.deleteById(id);
     }
